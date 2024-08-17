b73027684984bb6fb827291ce150018b4b00ee52
Arjun <arjundasofficial012@gmail.com>
2024-08-17 23:34:48 +0530

    creating two react projects and getting started the course

---
+++ 01basicreactproject/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Almec code | Arjun</h1>
+  );
+}
+
+export default App;
---
+++ 01reactproject/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  
+
+  return (
+    <h1>Chai aur Reacty with Vite | Almec</h1>
+  )
+}
+
+export default App
